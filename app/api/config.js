6a6579bc73c6df9eaa9e68bd9c0e4fa1683b529b
Nelsonlis <149626479+Nelsonlis@users.noreply.github.com>
2024-05-29 23:47:15 +0400

    0
    0

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://pengvinzxc.nomorepartiesco.ru//api'
+export const BASE_URL = 'http://https://hehado-dydy.nomorepartiesco.ru//api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,

d1001231e30e55978e763e0dcec1bd4ea22fb170
Nelsonlis <149626479+Nelsonlis@users.noreply.github.com>
2024-05-30 00:08:52 +0400

    0
    0

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://https://hehado-dydy.nomorepartiesco.ru//api'
+export const BASE_URL = 'https://https://hehado-dydy.nomorepartiesco.ru//api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
